d1725695b5de895adf38a9573a88ac1410e7dd5f
harryOdd <harryseptain003@gmail.com>
2026-05-27 23:23:03 +0300

    Refactor fetch and response handling in callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,37 +1,29 @@
 export default async function handler(req, res) {
   const { code } = req.query;
 
-  const response = await fetch(
-    "https://github.com/login/oauth/access_token",
-    {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        client_id: process.env.OAUTH_CLIENT_ID,
-        client_secret: process.env.OAUTH_CLIENT_SECRET,
-        code,
-      }),
-    }
-  );
+  const response = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      client_id: process.env.OAUTH_CLIENT_ID,
+      client_secret: process.env.OAUTH_CLIENT_SECRET,
+      code
+    })
+  });
 
   const data = await response.json();
 
-  const script = `
+  res.setHeader("Content-Type", "text/html");
+  res.send(`
     <script>
       window.opener.postMessage(
-        'authorization:github:success:${JSON.stringify({
-          token: data.access_token,
-          provider: "github"
-        })}',
-        '*'
+        "authorization:github:success:${JSON.stringify({ token: data.access_token })}",
+        "*"
       );
       window.close();
     </script>
-  `;
-
-  res.setHeader("Content-Type", "text/html");
-  res.send(script);
+  `);
 }
